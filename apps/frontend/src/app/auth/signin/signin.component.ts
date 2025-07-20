6400ebd66792d6a60b0dafc8dfef94572006977b
itapit <Itamardavid90@gmail.com>
2025-07-20 15:58:53 +0300

    added validators

--- apps/frontend/src/app/auth/signin/signin.component.ts
+++ apps/frontend/src/app/auth/signin/signin.component.ts
@@ -7,13 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './signin.component.css'
 })
 export class SigninComponent {
+
   title = 'Sign In';
+  buttonLabel = 'Sign In';
+  buttonIcon = 'pi pi-sign-in';
+
   emailLabel = 'Email';
   emailPlaceholder = 'Enter your email';
+  emailRequiredMsg = 'Email is required.';
+  emailInvalidMsg = 'Enter a valid email address.';
+
   passwordLabel = 'Password';
   passwordPlaceholder = 'Enter your password';
-  buttonLabel = 'Sign In';
-
+  passwordRequiredMsg = 'Password is required.';
+  passwordMinLength = 8;
+  passwordMinLengthMsg = `Password must be at least ${this.passwordMinLength} characters.`;
 
   email: string | undefined
   password: string | undefined
