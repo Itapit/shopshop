e873b733d01dda9917a2e30dafe5f1fbb6f0b03d
itapit <Itamardavid90@gmail.com>
2025-07-20 14:14:38 +0300

    added logo and search

--- apps/frontend/src/app/navbar/navbar-module.ts
+++ apps/frontend/src/app/navbar/navbar-module.ts
@@ -4,10 +4,14 @@ import { ButtonModule } from 'primeng/button';
 import { NavbarRoutingModule } from './navbar-routing-module';
 import { SignInLinkComponent } from './sign-in-link/sign-in-link.component';
 import { NavbarComponent } from './navbar.component';
+import { SearchProductsComponent } from './search-products/search-products.component';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { WebsiteLogoComponent } from './website-logo/website-logo.component';
 
 @NgModule({
-  declarations: [SignInLinkComponent, NavbarComponent],
-  imports: [CommonModule, NavbarRoutingModule, ButtonModule],
+  declarations: [SignInLinkComponent, NavbarComponent, SearchProductsComponent, WebsiteLogoComponent],
+  imports: [CommonModule, NavbarRoutingModule, ButtonModule, FormsModule, InputTextModule],
   exports: [NavbarComponent]
 })
 export class NavbarModule {}
---
+++ apps/frontend/src/app/navbar/website-logo/website-logo.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-website-logo',
+  standalone: false,
+  templateUrl: './website-logo.component.html',
+  styleUrl: './website-logo.component.css'
+})
+export class WebsiteLogoComponent {}
