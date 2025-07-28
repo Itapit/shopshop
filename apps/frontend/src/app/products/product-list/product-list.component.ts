96a9e869e0c68520fb8724fc358fbef50160f78e
itapit <Itamardavid90@gmail.com>
2025-07-28 14:44:43 +0300

    moved the pagination logic to product-list

--- apps/frontend/src/app/products/product-list/product-list.component.ts
+++ apps/frontend/src/app/products/product-list/product-list.component.ts
@@ -1,15 +1,43 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { productListOptionsEnum } from "./product-list-options-enum";
 import { ProductDto } from "@common/DTOs";
+import { Observable } from "rxjs";
+import { PaginatorState } from "primeng/paginator";
 @Component({
   selector: "app-product-list",
     standalone: false,
     templateUrl: "./product-list.component.html",
     styleUrls: ["./product-list.component.css"],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   @Input() mode!: productListOptionsEnum ; // 'view' or 'cart'
-  @Input() products!: ProductDto[];
+  @Input() fetchFunction!: (page: number, limit: number) => Observable<ProductDto[]>;
+  @Input() totalRecords!: number;
 
-  productListOptionsEnum = productListOptionsEnum;
+  productListOptionsEnum = productListOptionsEnum; //expose the enum to the html
+
+  products: ProductDto[] = [];
+  page = 1;
+  limit = 10;
+  
+  ngOnInit() {
+    this.loadProducts();
+  }
+
+  loadProducts(event?: PaginatorState) {
+    const page = (event?.page ?? 0) + 1;
+    const limit = event?.rows ?? this.limit;
+
+    this.page = page;
+    this.limit = limit;
+
+    this.fetchFunction(page, limit).subscribe({
+      next: (res) => {
+        this.products = res;
+      },
+      error: (err) => {
+        console.error('Error in fetchFunction:', err);
+      },
+    });
+  }
 }
