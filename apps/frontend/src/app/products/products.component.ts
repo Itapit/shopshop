1868cbb46686e7875460077ca5095a05b8cca4cf
itapit <Itamardavid90@gmail.com>
2025-07-28 10:31:13 +0300

    product page now request from back

--- apps/frontend/src/app/products/products.component.ts
+++ apps/frontend/src/app/products/products.component.ts
@@ -1,5 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { productListOptionsEnum } from "./product-list/product-list-options-enum";
+import { ProductsHttpService } from "./services/products-http.service";
+import { GetProductsListRequestDTO, GetProductsListResponseDTO } from "@common/DTOs";
 
 @Component({
     selector: "app-products",
@@ -7,81 +9,26 @@ import { productListOptionsEnum } from "./product-list/product-list-options-enum
     templateUrl: "./products.component.html",
     styleUrls: ["./products.component.css"],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
 
+  constructor(private productService: ProductsHttpService) {}
   productListOptionsEnum = productListOptionsEnum; //expose the enum to the html
+  productsResponse: GetProductsListResponseDTO | null = null
 
-    products = [
-    {
-      name: 'Bamba',
-      price: 4.99,
-      description: 'Classic peanut snack.',
-      quantity: 30,
-      imageUrl: 'assets/images/bamba.png'
-    },
-    {
-      name: 'Chocolate Bar',
-      price: 6.5,
-      description: 'High-quality milk chocolate.',
-      quantity: 12,
-      imageUrl: 'https://images.weserv.nl/?url=images.snack/2.jpg'
-    },
-    {
-      name: 'Coca-Cola Can',
-      price: 3.5,
-      description: 'Refreshing fizzy drink.',
-      quantity: 24,
-      imageUrl: 'https://images.weserv.nl/?url=images.snack/3.jpg'
-    },
-    {
-      name: 'Potato Chips',
-      price: 5.0,
-      description: 'Crunchy salted chips.',
-      quantity: 50,
-      imageUrl: 'https://images.weserv.nl/?url=images.snack/4.jpg'
-    },
-    {
-      name: 'Energy Drink',
-      price: 8.0,
-      description: 'Boost your energy anytime.',
-      quantity: 18,
-      imageUrl: 'https://images.weserv.nl/?url=images.snack/5.jpg'
-    },
-    {
-      name: 'Orange Juice',
-      price: 7.0,
-      description: 'Freshly squeezed orange juice.',
-      quantity: 10,
-      imageUrl: 'https://images.weserv.nl/?url=images.snack/6.jpg'
-    },
-    {
-      name: 'Chocolate Cookies',
-      price: 9.99,
-      description: 'Bite-sized chocolate chip cookies.',
-      quantity: 40,
-      imageUrl: 'assets/images/bamba.png'
-    },
-    {
-      name: 'Mint Gum',
-      price: 2.99,
-      description: 'Long-lasting mint flavor.',
-      quantity: 100,
-      imageUrl: 'https://images.weserv.nl/?url=images.snack/8.jpg'
-    },
-    {
-      name: 'Granola Bar',
-      price: 3.5,
-      description: 'Healthy and delicious snack.',
-      quantity: 60,
-      imageUrl: 'https://images.weserv.nl/?url=images.snack/9.jpg'
-    },
-    {
-      name: 'Strawberry Yogurt',
-      price: 4.0,
-      description: 'Creamy strawberry flavored yogurt.',
-      quantity: 20,
-      imageUrl: 'https://images.weserv.nl/?url=images.snack/10.jpg'
+  ngOnInit(): void {
+    const requestDto: GetProductsListRequestDTO = { //TODO fix this hardcoded so the user can select page
+      page:1,
+      limit:10
     }
-  ];
 
+    this.productService.getProducts(requestDto).subscribe({
+      next: (response) => {
+        this.productsResponse = response;
+        console.log('products loaded:', response);
+      },
+      error: (error) => {
+        console.error('not good at all', error);
+      }
+    });
+  }
 }
