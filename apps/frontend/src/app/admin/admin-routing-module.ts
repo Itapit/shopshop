52a9811334294cdb13abee6d5d53432afee8f2f9
itapit <Itamardavid90@gmail.com>
2025-08-04 13:12:19 +0300

    Added admin guard to the stats page

--- apps/frontend/src/app/admin/admin-routing-module.ts
+++ apps/frontend/src/app/admin/admin-routing-module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OrdersStatsComponent } from './orders-stats/orders-stats.component';
+import { AdminGuard } from '../auth/guards/admin.guard';
 
 const routes: Routes = [
-  {path: 'stats', component:OrdersStatsComponent},
+  {path: 'stats', component:OrdersStatsComponent, canActivate:[AdminGuard]},
 ];
 
 @NgModule({
