e7b1dbdf03c1c8717671f16c27b7b6af2557c7cf
RonenShot <ronenshot2006@gmail.com>
2025-08-04 15:45:52 +0300

    reload the cart omponent after changing the cart.

--- apps/frontend/src/app/products/product-card/product-card.component.ts
+++ apps/frontend/src/app/products/product-card/product-card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from "@angular/core";
 import {  ProductFull, ProductItem } from "@common/Interfaces";
 import { productListOptionsEnum } from "../product-list/product-list-options-enum";
 import { CartService } from "../../cart/services/cart.service";
+import { Route, Router } from "@angular/router";
 
 @Component({
   selector: "app-product-card",
@@ -14,14 +15,18 @@ export class ProductCardComponent{
   @Input() product!: ProductFull;
   
   quantity = 0;
-  constructor(private cartService: CartService){}
+  constructor(private cartService: CartService , private router : Router){}
 
-  productListOptionsEnum = productListOptionsEnum; //expose the enum to the html
+  productListOptionsEnum = productListOptionsEnum; 
   removeItemFromCart(){
      console.log("productID", this.product.productID , "  product: " , this.product);
      const item : ProductItem = {productID: this.product.productID , quantity: this.quantity};
      console.log("remove item:" ,item);
      console.log("ojsndjons");
      this.cartService.updateCartItemQuantity(item);
+     this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+      this.router.navigate(['cart']);
+     });
   }
-}
+ }
+
