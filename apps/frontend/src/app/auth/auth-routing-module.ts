35d57dc0442753b45134b715d3b14058dda5f912
itapit <Itamardavid90@gmail.com>
2025-08-04 10:14:08 +0300

    Added create user option when your admin

--- apps/frontend/src/app/auth/auth-routing-module.ts
+++ apps/frontend/src/app/auth/auth-routing-module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SigninComponent } from './signin/signin.component';
+import { SigninComponent } from './sign-in/signin.component';
+import { CreateUserComponent } from './create-user/create-user.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'signin', pathMatch: 'full'},
-  { path: 'signin', component: SigninComponent }
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: CreateUserComponent}
 ];
 
 @NgModule({
